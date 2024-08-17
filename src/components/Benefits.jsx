c4735551c29772ff2194452fb81eb651f0fef61c
Mateusz Leśniak <mateusz.lesniakk@gmail.com>
2024-08-17 21:28:58 +0200

    finish benefits

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -4,7 +4,9 @@ import Section from "./Section";
 
 import Arrow from "../assets/svg/Arrow";
 
-import {GradientLight} from "./design/Benefits";
+import { GradientLight } from "./design/Benefits";
+
+import ClipPath from "../assets/svg/ClipPath";
 
 const Benefits = () => {
   return (
@@ -41,6 +43,25 @@ const Benefits = () => {
                 </div>
               </div>
               {item.light && <GradientLight />}
+
+              <div
+                className="absolute inset-0.5 bg-n-8 "
+                style={{ clipPath: "url(benefits" }}
+              >
+                <div className="absolute inset-0 opacity-0 transition-opacity hover:opacity-10">
+                  {item.imageUrl && (
+                    <img
+                      src={item.imageUrl}
+                      width={380}
+                      height={362}
+                      alt={item.title}
+                      className="w-full h-full object-cover"
+                    />
+                  )}
+                </div>
+              </div>
+
+              <ClipPath />
             </div>
           ))}
         </div>
