225fa07d4f1a0091277a6ef8f14da55c72955569
Mateusz Leśniak <mateusz.lesniakk@gmail.com>
2024-08-23 22:57:38 +0200

    Add pricing and tagline component

---
+++ src/components/TagLine.jsx
@@ -0,0 +1,10 @@
+import brackets from "../assets/svg/Brackets";
+export const TagLine = ({ className, children }) => {
+  return (
+    <div className={`tagline, flex items-center ${className || ""}`}>
+      {brackets("left")}
+      <div className="mx-3 text-n-3">{children}</div>
+      {brackets("right")}
+    </div>
+  );
+};
