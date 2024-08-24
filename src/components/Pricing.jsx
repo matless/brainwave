a5f431feda53e27a75ffe123b58977e27f1d501a
Mateusz Leśniak <mateusz.lesniakk@gmail.com>
2024-08-24 22:17:47 +0200

    add PricingList and finish Pricing section

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,7 +1,8 @@
-import React from "react";
 import Section from "./Section";
 import { smallSphere, stars } from "../assets";
 import Heading from "./Heading";
+import PricingList from "./PricingList";
+import { LeftLine, RightLine } from "./design/Pricing";
 
 const Pricing = () => {
   return (
@@ -31,7 +32,20 @@ const Pricing = () => {
           title="Pay once, use forever"
         />
 
-        <div className="relative"></div>
+        <div className="relative">
+          <PricingList />
+          <LeftLine />
+          <RightLine />
+        </div>
+
+        <div className="flex justify-center mt-10">
+          <a
+            className="text-xs font-code font-bold tracking-wider uppercase border-b"
+            href="/pricing"
+          >
+            See the full details
+          </a>
+        </div>
       </div>
     </Section>
   );
